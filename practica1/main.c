d428639828df66b318b94bc5b8a06f92f22868d1
anitajv <janikosky@gmail.com>
2015-02-23 10:37:34 +0100

    Update main.c

--- practica1/main.c
+++ practica1/main.c
@@ -1,4 +1,202 @@
-#include <stdlib.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <time.h>
+#include <signal.h>
+#include <wiringPi.h>
 #include "fsm.h"
 
-enum t_estado{reposo, e_vaso, e_cafe, e_leche};
+#define GPIO_BUTTON	2
+#define GPIO_LED	3
+#define GPIO_CUP	4
+#define GPIO_COFFEE	5
+#define GPIO_MILK	6
+#define GPIO_MONEY      7
+#define GPIO_VUELTAS    8
+
+#define CUP_TIME	250
+#define COFFEE_TIME	3000
+#define MILK_TIME	3000
+
+#define PRECIO         0.50
+
+
+enum cofm_state {
+  COFM_WAITING,
+  COFM_CUP,
+  COFM_COFFEE,
+  COFM_MILK,
+  COFM_MONEY,
+  COFM_VUELTAS,
+};
+
+static int button = 0;
+static void button_isr (void) { button = 1; }
+
+static int money = 0;
+static void money_isr (void) { money = 1; }    //INTERRUPCIÓN PARA LA ENTRADA DE MONEDAS
+
+
+static int timer = 0;
+static void timer_isr (union sigval arg) { timer = 1; }
+static void timer_start (int ms)
+{
+  timer_t timerid;
+  struct itimerspec value;
+  struct sigevent se;
+  se.sigev_notify = SIGEV_THREAD;
+  se.sigev_value.sival_ptr = &timerid;
+  se.sigev_notify_function = timer_isr;
+  se.sigev_notify_attributes = NULL;
+  value.it_value.tv_sec = ms / 1000;
+  value.it_value.tv_nsec = (ms % 1000) * 1000000;
+  value.it_interval.tv_sec = 0;
+  value.it_interval.tv_nsec = 0;
+  timer_create (CLOCK_REALTIME, &se, &timerid);
+  timer_settime (timerid, 0, &value, NULL);
+}
+
+static int button_pressed (fsm_t* this)
+{
+  int ret = button;
+  button = 0;
+  return ret;
+}
+
+static int money (fsm_t* this)
+{
+  int ret = money;
+  money = 0;
+  return ret;
+
+  int cuenta = cuenta + moneda;       //NO SE HACER UN BUFER QUE VAYA ALMACENANDO LOS VALORES DE LAS MONEDAS, PERO ESTA ES LA IDEA
+
+}
+
+static int timer_finished (fsm_t* this)
+{
+  int ret = timer;
+  timer = 0;
+  return ret;
+}
+
+static void cup (fsm_t* this)
+{
+  digitalWrite (GPIO_LED, LOW);
+  digitalWrite (GPIO_CUP, HIGH);
+  timer_start (CUP_TIME);
+}
+
+static void coffee (fsm_t* this)
+{
+  digitalWrite (GPIO_CUP, LOW);
+  digitalWrite (GPIO_COFFEE, HIGH);
+  timer_start (COFFEE_TIME);
+}
+
+static void milk (fsm_t* this)
+{
+  digitalWrite (GPIO_COFFEE, LOW);
+  digitalWrite (GPIO_MILK, HIGH);
+  timer_start (MILK_TIME);
+}
+
+
+static void vueltas(fsm_t* this)
+{
+  digitalWrite (GPIO_VUELTAS, HIGH);    //TE DICE SI SE HAN DEVUELTO LAS VUELTAS (HIGH) O NO.
+  digitalWrite (GPIO_LED, HIGH);
+
+	int cambio= PRECIO - //cuenta;   //VALOR ABSOLUTO
+         return cambio;
+
+}
+
+
+static void finish (fsm_t* this)
+{
+  digitalWrite (GPIO_MILK, LOW);
+  digitalWrite (GPIO_LED, HIGH);
+}
+
+
+
+
+// Explicit FSM description
+static fsm_trans_t cofm[] = {
+  { COFM_WAITING, button_pressed, COFM_CUP,     cup    },
+  { COFM_CUP,     timer_finished, COFM_COFFEE,  coffee },
+  { COFM_COFFEE,  timer_finished, COFM_MILK,    milk   },
+  { COFM_MILK,    timer_finished, COFM_WAITING, finish },
+  {-1, NULL, -1, NULL },
+};
+
+static fsm_trans_t cofm2[] = {
+  { COFM_MONEY, money, COFM_VUELTAS, vueltas},
+  { COFM_VUELTAS, milk, COFM_MONEY, finish },   //DEVUELVE EL DINERO CUANDO HA ACABADO DE ECHAR LA LECHE
+  {-1, NULL, -1, NULL },
+};
+
+
+
+
+// Utility functions, should be elsewhere
+
+// res = a - b
+void
+timeval_sub (struct timeval *res, struct timeval *a, struct timeval *b)
+{
+  res->tv_sec = a->tv_sec - b->tv_sec;
+  res->tv_usec = a->tv_usec - b->tv_usec;
+  if (res->tv_usec < 0) {
+    --res->tv_sec;
+    res->tv_usec += 1000000;
+  }
+}
+
+// res = a + b
+void
+timeval_add (struct timeval *res, struct timeval *a, struct timeval *b)
+{
+  res->tv_sec = a->tv_sec + b->tv_sec
+    + a->tv_usec / 1000000 + b->tv_usec / 1000000; 
+  res->tv_usec = a->tv_usec % 1000000 + b->tv_usec % 1000000;
+}
+
+// wait until next_activation (absolute time)
+void delay_until (struct timeval* next_activation)
+{
+  struct timeval now, timeout;
+  gettimeofday (&now, NULL);
+  timeval_sub (&timeout, next_activation, &now);
+  select (0, NULL, NULL, NULL, &timeout);
+}
+
+
+
+int main ()
+{
+  struct timeval clk_period = { 0, 250 * 1000 };
+  struct timeval next_activation;
+  fsm_t* cofm_fsm = fsm_new (cofm);
+
+  wiringPiSetup();
+  pinMode (GPIO_BUTTON, INPUT);
+  pinMode (GPIO_MONEY, INPUT);
+  wiringPiISR (GPIO_BUTTON, INT_EDGE_FALLING, button_isr);
+  wiringPiISR (GPIO_MONEY, INT_EDGE_FALLING, money_isr);
+  pinMode (GPIO_VUELTAS, OUTPUT);
+  pinMode (GPIO_CUP, OUTPUT);
+  pinMode (GPIO_COFFEE, OUTPUT);
+  pinMode (GPIO_MILK, OUTPUT);
+  pinMode (GPIO_LED, OUTPUT);
+  digitalWrite (GPIO_LED, HIGH);
+  
+  gettimeofday (&next_activation, NULL);
+  while (1) {
+    fsm_fire (cofm_fsm);
+    timeval_add (&next_activation, &next_activation, &clk_period);
+    delay_until (&next_activation);
+  }
+
+
+}
